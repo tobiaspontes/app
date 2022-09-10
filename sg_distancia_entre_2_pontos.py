cb676897026c302c0f48355e0f57b1106c137c4a
João Tobias <tobiaspontes@gmail.com>
2022-09-10 15:22:36 -0300

    Update sg_distancia_entre_2_pontos.py

--- sg_distancia_entre_2_pontos.py
+++ sg_distancia_entre_2_pontos.py
@@ -8,15 +8,6 @@ def calcula_distancia(x1,y1,x2,y2):
 	return math.sqrt(((x1 - x2) ** 2) + ((y1 - y2) ** 2))
 
 
-# if (__name__ == '__main__'):
-# 	x1 = float(input('\nDigite a abcissa do primeiro ponto: '))
-# 	y1 = float(input('Digite a ordenada do primeiro ponto: '))
-# 	x2 = float(input('Digite a abcissa do segundo ponto: '))
-# 	y2 = float(input('Digite a ordenada do segundo ponto: '))
-# 	distancia = calcula_distancia(x1,y1,x2,y2)
-# 	print(f'\nA distância entre os pontos ({x1},{y1}) e ({x2},{y2}) é de {distancia:.2f}.')
-
-
 sg.theme('BluePurple')
 layout = [
     [sg.Text('Primeiro ponto'), sg.Push(), sg.Text('Segundo ponto')],
